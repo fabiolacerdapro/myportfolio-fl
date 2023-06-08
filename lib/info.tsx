1d29da5e973d4240a901b943f8766ff170e2806d
Fabio Lacerda <fabiolaccerd01@gmail.com>
2023-06-08 06:55:28 -0300

    New New

--- lib/info.tsx
+++ lib/info.tsx
@@ -12,7 +12,7 @@ export const about = () => {
 export const bio = () => {
   return (
     <>
-      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
-    </>
+      Mais e mais testes para veroficar se os meus commits vão aparecer no meu perfil.
+    </> 
   );
 };
